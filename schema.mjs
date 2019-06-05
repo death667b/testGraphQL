ab9b9a9940dafbbbeea06ee91ac85b2af39be2c2
death667b <idaniel@spinnys.com>
2019-06-05 18:51:18 +1000

    Created graphQL schema

---
+++ schema.mjs
@@ -0,0 +1,46 @@
+import {
+  graphql,
+  GraphQLScheme,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLList
+} from "graphql";
+import people from './data/people';
+
+let humanType = new GraphQLObjectType({
+  name: "Human",
+  fields: () => ({
+    id: { type: GraphQLString },
+    description: { type: GraphQLString },
+    name: { type: GraphQLString }
+  })
+});
+
+let schema = new GraphQLScheme({
+  query: new GraphQLObjectType({
+    name: "RootQueryType",
+    fields: {
+      hello: {
+        type: GraphQLString,
+        resolve() {
+          return "World";
+        }
+      },
+      person: {
+        type: humanType,
+        resolve() {
+          return people[0];
+        }
+      },
+      people: {
+        type: new GraphQLList(humanType),
+        resolve() {
+          return people;
+        }
+      }
+    }
+  })
+});
+
+export { graphql };
+export default schema;
