aaa8a21a024a2551cb909052cac2fc80bb9928a6
death667b <idaniel@spinnys.com>
2019-06-05 19:10:32 +1000

    Add two data points to the people database

--- data/people.js
+++ data/people.js
@@ -1,1 +1,14 @@
-export const people = [];
+const people = [
+  {
+    id: 'abc123',
+    description: 'asdbcasdbc asdbc d',
+    name: 'joe joe'
+  },
+  {
+    id: '12345667-f',
+    description: 'this is a test desc',
+    name: 'bob bobbins'
+  }
+];
+
+module.exports = people;
